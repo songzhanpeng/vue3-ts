7ebbc5fe6d238e34040b77baf43fc0f8ea0e1064
songzhanpeng <songzhanpeng@iauto.com>
2023-05-06 10:38:47 +0800

    feat: 初始化

---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>About</div>
+</template>
+
+<script setup lang="ts">
+</script>
+<style scoped lang='scss'>
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import autoImport from "unplugin-auto-import/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    autoImport({
+      imports: ["vue", "vue-router"],
+    }),
+  ],
+});
